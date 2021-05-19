dbc90a54a058faa4f0792e640624ae492d6adea1
AndreaDex <desiderio.andrea@outlook.it>
2021-05-19 11:38:37 +0200

    add autoslide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 /* Descrizione:
- *Creare uno slider di immagini: potete usare le immagini che desiderate.
- *Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
- *Inoltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera (usate un event listener su document e ascoltate per il keyup)
- *Utiliziamo una classe first e last per capire quali sono la prima e ultima immagine dello slider
- *Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider
- *Clicchiamo sui pallini e mostriamo l’immagine corrispondente
+ Creare uno slider di immagini: potete usare le immagini che desiderate.
+ Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
+ Inoltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera (usate un event listener su document e ascoltate per il keyup)
+ Utiliziamo una classe first e last per capire quali sono la prima e ultima immagine dello slider
+ Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider
+ Clicchiamo sui pallini e mostriamo l’immagine corrispondente
  */
 const app = new Vue({
   el: "#app",
+
   data: {
     counter: 0,
     gallery: [
@@ -18,6 +19,7 @@ const app = new Vue({
       "./assets/img/we_still_have_around.jpg",
     ],
   },
+
   methods: {
     prev() {
       if (this.counter <= 0) {
@@ -35,21 +37,16 @@ const app = new Vue({
       this.counter = index;
     },
   },
+
   mounted() {
-    console.log("ciao");
     window.addEventListener("keydown", (e) => {
-      console.log(e.key);
       if (e.key === "ArrowRight") {
-        if (this.counter >= this.gallery.length - 1) {
-          return (this.counter = 0);
-        }
-        return this.counter++;
+        this.next();
       } else if (e.key === "ArrowLeft") {
-        if (this.counter <= 0) {
-          return (this.counter = this.gallery.length - 1);
-        }
-        return this.counter--;
+        this.prev();
       }
     });
+
+    setInterval(this.next, 3000);
   },
 });
